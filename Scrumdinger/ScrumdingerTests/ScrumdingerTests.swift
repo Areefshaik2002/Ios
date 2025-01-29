2ec8b96251381423c180353cfc65bca54e7f2e2d
Shaik Areef <Areefshaik2002@gmail.com>
2025-01-29 11:45:21 +0530

    scrumdinger

---
+++ Scrumdinger/ScrumdingerTests/ScrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerTests.swift
+//  ScrumdingerTests
+//
+//  Created by Shaik Areef on 28/01/25.
+//
+
+import Testing
+@testable import Scrumdinger
+
+struct ScrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
